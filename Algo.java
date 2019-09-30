b92b55590f3b1c3a8808b285f45e1c7035826a28
Saskia Imani <imani.saskia@gmail.com>
2019-09-30 15:20:41 +0700

    Initialized Algo class

---
+++ Algo.java
@@ -0,0 +1,22 @@
+public class Algo {
+    /** ----- PRIVATE METHODS ----- */
+    /** Generates legal moves based on board matrix */
+    private static tuple[] genLegalMoves(String[][] board) {
+        tuple legal_moves[] = new tuple[60];
+        return legal_moves;
+    }
+
+
+
+    /** ----- PUBLIC METHODS ----- */
+    /** Generates random move from set of legal moves */
+    public static tuple genRandomMove(String[][] board) {
+        tuple move = new tuple(0,0);
+        return move;
+    }
+    /** Generates minimax move from set of legal moves */
+    public static tuple genMinimaxMove(String[][] board, boolean isPlayer) {
+        tuple move = new tuple(0,0);
+        return move;
+    }
+}
