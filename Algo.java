1656ce6c5e6c6e9e5e644290dfc07e0e0b652939
Saskia Imani <imani.saskia@gmail.com>
2019-10-01 16:03:29 +0700

    Added method Algo.genLevalMoves

--- Algo.java
+++ Algo.java
@@ -1,10 +1,33 @@
-import java.util.Random;
+import java.util.*;
 
 public class Algo {
     /** ----- PRIVATE METHODS ----- */
     /** Generates legal moves based on board matrix */
     private static tuple[] genLegalMoves(State s) {
-        tuple legal_moves[] = new tuple[60];
+        List<tuple> list = new ArrayList<tuple>(); //no fixed size mentioned
+        
+        for (int i = 1; i < 9; i++) {
+            for (int j = 1; j < 9; j++) {
+                tuple t = new tuple(i, j);    
+                /** If there is a line from t to any direction */
+                if ((s.findALine(t, "up").i != 0 && s.findALine(t, "up").j != 0)
+                || (s.findALine(t, "up-right").i != 0 && s.findALine(t, "up-right").j != 0)
+                || (s.findALine(t, "up-left").i != 0 && s.findALine(t, "up-left").j != 0)
+                || (s.findALine(t, "right").i != 0 && s.findALine(t, "right").j != 0)
+                || (s.findALine(t, "left").i != 0 && s.findALine(t, "left").j != 0)
+                || (s.findALine(t, "down").i != 0 && s.findALine(t, "down").j != 0)
+                || (s.findALine(t, "down-right").i != 0 && s.findALine(t, "down-right").j != 0)
+                || (s.findALine(t, "down-left").i != 0 && s.findALine(t, "down-left").j != 0)
+                ) {
+                    list.add(t);
+                }
+            }
+        }
+
+        tuple[] legal_moves = new tuple[list.size()];
+        for (int x = 0; x < list.size(); x++) {
+            legal_moves[x] = list.get(x);
+        }
         return legal_moves;
     }
 
