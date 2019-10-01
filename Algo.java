b8af5ebf80d99202e1b4ef81a0b9fac7fa4e21fc
Saskia Imani <imani.saskia@gmail.com>
2019-10-01 14:16:49 +0700

    Changed function parameters to State Instead of State.board

--- Algo.java
+++ Algo.java
@@ -3,23 +3,22 @@ import java.util.Random;
 public class Algo {
     /** ----- PRIVATE METHODS ----- */
     /** Generates legal moves based on board matrix */
-    private static tuple[] genLegalMoves(String[][] board) {
+    private static tuple[] genLegalMoves(State s) {
         tuple legal_moves[] = new tuple[60];
         return legal_moves;
     }
 
 
-
     /** ----- PUBLIC METHODS ----- */
     /** Generates random move from set of legal moves */
-    public static tuple genRandomMove(String[][] board) {
-        tuple legal_moves[] = genLegalMoves(board);
+    public static tuple genRandomMove(State s) {
+        tuple legal_moves[] = genLegalMoves(s);
         int random_index = new Random().nextInt(legal_moves.length);
         return legal_moves[random_index];
     }
     
     /** Generates minimax move from set of legal moves */
-    public static tuple genMinimaxMove(String[][] board, boolean isPlayer) {
+    public static tuple genMinimaxMove(State s, boolean isPlayer) {
         tuple move = new tuple(0,0);
         return move;
     }
