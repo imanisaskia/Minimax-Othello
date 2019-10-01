fb58d9175de033a51175e81bb4482c351313d9fb
elvinaang <13517079@std.stei.itb.ac.id>
2019-10-01 16:08:25 +0700

    minimax + eval

--- Algo.java
+++ Algo.java
@@ -1,7 +1,16 @@
 import java.util.*; 
 
 public class Algo {
+
+    private static tuple best_coordinate = new tuple(0, 0);   
+
     /** ----- PRIVATE METHODS ----- */
+
+    private static void setBestCoordinate(int i, int j) {
+      best_coordinate.i = i;
+      best_coordinate.j = j;
+    }
+
     /** Generates legal moves based on board matrix */
     private static tuple[] genLegalMoves(State s) {
         tuple legal_moves[] = new tuple[60];
@@ -17,16 +26,24 @@ public class Algo {
     }
 
     /** Generates minimax move from set of legal moves */
+    public static tuple genMinimaxMove(State s, boolean isPlayer) {
+      tuple legal_moves[] = genLegalMoves(s);
+      int alpha = Integer.MIN_VALUE;
+      int beta = Integer.MAX_VALUE;
+
+      int score = minimax(s, 3, isPlayer, alpha, beta);
+
+      return legal_moves[0];
+    }
 
     private static int minimax(State curr_state, int depth, boolean isPlayer, int alpha, int beta) {
         tuple move = new tuple(0,0);
         tuple legal_moves[] = genLegalMoves(curr_state);
         int n_legal_moves = legal_moves.length;
         int best_move_score;
-        tuple best_move_coordinate;
 
         if (depth == 0) { /**initiation**/
-          return evaluation(curr_state.getBoard());
+          return evaluation(curr_state, isPlayer);
         }
         else if (isPlayer) { /** opponent's move **/
           best_move_score = Integer.MIN_VALUE;
@@ -37,7 +54,7 @@ public class Algo {
             int score = minimax(new_state, depth-1, !isPlayer, alpha, beta);
             if (score > best_move_score) {
               best_move_score = score;
-              best_move_coordinate = move;
+              setBestCoordinate(move.i, move.j);
             }
             set_alpha(alpha, best_move_score); /** alpha beta pruning */
             if (beta <= alpha) {
@@ -54,7 +71,7 @@ public class Algo {
             int score = minimax(new_state, depth-1, !isPlayer, alpha, beta);
             if (score < best_move_score) {
               best_move_score = score;
-              best_move_coordinate = move;
+              setBestCoordinate(move.i, move.j);
             }
             set_beta(beta, best_move_score); /** alpha beta pruning */
             if (beta <= alpha) {
@@ -62,7 +79,6 @@ public class Algo {
             }
           }
         }
-
         return best_move_score;
     }
 
@@ -80,13 +96,35 @@ public class Algo {
 
     /** EVALUATION **/
 
-    private static int evaluation(State curr_state) {
+    private static int evaluation(State curr_state, Boolean isPlayer) {
+      tuple coordinate = new tuple(0,0);
+      String checking_color;
       int score = 0;
-      
-      if(is_corner(coordinate)) {
-        score += 50;
-      }
 
+      if (isPlayer) {
+        checking_color = "0";
+      }
+      else {
+        checking_color = "1";
+      }
+      
+      for (int i = 1; i < 8; i++) {
+        for (int j = 1; j < 8; j++) {
+          coordinate.i = i;
+          coordinate.j = j;
+          if (curr_state.getBoard(i,j) == checking_color) { /* Cek apakah warnanya hitam/putih */
+            if (is_corner(coordinate)) {
+              score += 50;
+            }
+            else if (is_vertices(coordinate)) {
+              score += 20;
+            }
+            else {
+              score += 1;
+            }
+          }
+        }
+      }
       return score;
     }
 
@@ -108,11 +146,22 @@ public class Algo {
       }
     }
 
-    private static int countFlipped(String[][] board) {
-      int flipped = 0;
-
-      return flipped;
+    private static boolean is_vertices(tuple coordinate) {
+      if ((coordinate.i == 1)) {
+        return true;
+      }
+      else if ((coordinate.i == 8)) {
+        return true;
+      }
+      else if ((coordinate.j == 1)) {
+        return true;
+      }
+      else if ((coordinate.j == 8)) {
+        return true;
+      }
+      else {
+        return false;
+      }
     }
-
 
 }
